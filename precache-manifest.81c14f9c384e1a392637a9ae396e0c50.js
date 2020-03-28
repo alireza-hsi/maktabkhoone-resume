0a9b08b3bf99eb2e1292b76fd2d1954690e9c119
alireza-hsi <alireza.houshidari@gmail.com>
2020-03-28 20:43:43 +0430

    Updates

---
+++ precache-manifest.81c14f9c384e1a392637a9ae396e0c50.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02bb412db49696b3c1f4b12ee215dd42",
+    "url": "/maktabkhoone-resume/index.html"
+  },
+  {
+    "revision": "f11efe1814caafc7f67e",
+    "url": "/maktabkhoone-resume/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "ef798ead56f7efeb4e10",
+    "url": "/maktabkhoone-resume/static/css/main.de74ff65.chunk.css"
+  },
+  {
+    "revision": "f11efe1814caafc7f67e",
+    "url": "/maktabkhoone-resume/static/js/2.85604fcc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/maktabkhoone-resume/static/js/2.85604fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef798ead56f7efeb4e10",
+    "url": "/maktabkhoone-resume/static/js/main.01f76040.chunk.js"
+  },
+  {
+    "revision": "d03aafdb2cff06a04b0b",
+    "url": "/maktabkhoone-resume/static/js/runtime-main.44529cb9.js"
+  },
+  {
+    "revision": "c1d042b4956aa1fa794eb3d97e1c0eff",
+    "url": "/maktabkhoone-resume/static/media/react 2.c1d042b4.png"
+  },
+  {
+    "revision": "ef361b687381c27d5bc8a0e4874a5720",
+    "url": "/maktabkhoone-resume/static/media/sass1.ef361b68.jpg"
+  }
+]);
